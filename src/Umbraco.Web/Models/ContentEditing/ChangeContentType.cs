7e82524c4e2efd5358eb4fc4e0be396ce14d07dd
Warren <warren@umbraco.com>
2018-08-13 12:18:42 +0100

    Camel case properties to make Mads not have a OCD issue with the server responses :)

--- src/Umbraco.Web/Models/ContentEditing/ChangeContentType.cs
+++ src/Umbraco.Web/Models/ContentEditing/ChangeContentType.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using Umbraco.Core.Models;
 
 namespace Umbraco.Web.Models.ContentEditing
 {
+    [DataContract(Name = "changeContentType", Namespace = "")]
     public class ChangeContentType
     {
+        [DataMember(Name = "contentNodeId")]
         public int ContentNodeId { get; set; }
 
+        [DataMember(Name = "newContentTypeId")]
         public int NewContentTypeId { get; set; }
 
+        [DataMember(Name = "newTemplateId")]
         public int NewTemplateId { get; set; }
 
+        [DataMember(Name = "fieldMap")]
         public IEnumerable<FieldMap> FieldMap { get; set; }
     }
 
+    [DataContract(Name = "fieldMap", Namespace = "")]
     public class FieldMap
     {
+        [DataMember(Name = "fromAlias")]
         public string FromAlias { get; set; }
 
+        [DataMember(Name = "toAlias")]
         public string ToAlias { get; set; }
     }
 
@@ -28,36 +37,48 @@ namespace Umbraco.Web.Models.ContentEditing
         public object CurrentValue { get; set; }
     }
 
-
+    [DataContract(Name = "availableContentTypes", Namespace = "")]
     public class AvailableContentTypes
     {
+        [DataMember(Name = "currentNodeName")]
         public string CurrentNodeName { get; set; }
 
+        [DataMember(Name = "currentContentType")]
         public IContentType CurrentContentType { get; set; }
 
+        [DataMember(Name = "contentTypes")]
         public IEnumerable<IContentType> ContentTypes { get; set; }
     }
 
+    [DataContract(Name = "availableProperties", Namespace = "")]
     public class AvailableProperties
     {
+        [DataMember(Name = "templates")]
         public IEnumerable<ITemplate> Templates { get; set; }
 
+        [DataMember(Name = "currentProperties")]
         public IEnumerable<CurrentProperty> CurrentProperties { get; set; }
     }
 
+    [DataContract(Name = "currentProperty", Namespace = "")]
     public class CurrentProperty
     {
+        [DataMember(Name = "propertyName")]
         public string PropertyName { get; set; }
 
+        [DataMember(Name = "propertyAlias")]
         public string PropertyAlias { get; set; }
 
+        [DataMember(Name = "allowed")]
         public IEnumerable<NewProperty> Allowed { get; set; }
     }
 
     public class NewProperty
     {
+        [DataMember(Name = "propertyName")]
         public string PropertyName { get; set; }
 
+        [DataMember(Name = "propertyAlias")]
         public string PropertyAlias { get; set; }
     }
 }
